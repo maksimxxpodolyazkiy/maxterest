331cb6304414fa70b9651b061fd6c10526144350
Maksim Podolyazkiy <maksim.podolyazkiy@gmail.com>
2019-11-28 13:44:59 +0300

    ssh-test

--- src/app/collections/collections.component.ts
+++ src/app/collections/collections.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnChanges, SimpleChanges } from "@angular/core";
+
+interface Collection {
+  id: any
+}
 
 @Component({
   templateUrl: "./collections.component.html",
   styleUrls: ["./collections.component.scss"]
 })
-export class CollectionsComponent implements OnInit {
+export class CollectionsComponent implements OnInit, OnChanges {
   onLogged = false;
 
   collections = [];
+  collection: Collection = {
+    id: Math.floor(Math.random() * 1000000)
+  }
 
   isVisible = false;
   isOkLoading = false;
@@ -16,12 +23,19 @@ export class CollectionsComponent implements OnInit {
 
   ngOnInit() {}
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(changes);
+    
+  }
+
+
   showModal(): void {
     this.isVisible = true;
   }
 
   handleOk(): void {
     this.isOkLoading = true;
+    Object.assign(this.collection)
     setTimeout(() => {
       this.isVisible = false;
       this.isOkLoading = false;
@@ -32,11 +46,12 @@ export class CollectionsComponent implements OnInit {
     this.isVisible = false;
   }
 
-  onAddCollection() {
+  onAddCollection(): void {
     if (!this.onLogged) {
       this.onLogged = true;
     }
-    const collection = {};
-    this.collections.push(collection);
+
+    this.collections.push(this.collection);
+    
   }
 }
